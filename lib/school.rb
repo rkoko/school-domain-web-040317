738d04f3d333cd973ddb17b8c0d01e29cdde9e18
rkoko <rinda.ko@gmail.com>
2017-05-16 11:46:23 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -10,24 +10,18 @@ class School
     @roster
   end
 
-  def add_student(student, grade)
-    if @roster.has_key?(grade)
-      @roster[grade]<<student
-    else
-      @roster[grade]=[]
-      @roster[grade]<<student
-    end
+def add_student(student, grade)
+  roster[grade] ||= []
+  roster[grade] << student
+end
+
+def grade(grade)
+  roster[grade]
+end
+
+def sort
+  roster.each do |grade, student|
+  student.sort!
   end
-
-  def grade(grade)
-    @roster[grade]
-  end
-
-  def sort
-    @roster.each do |age, student|
-      student.sort!
-    end
-  end
-
-
+end
 end
